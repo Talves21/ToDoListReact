ebfcfa12e11b16ff9d86a9f360a2493bbe572eb9
Terrell-Alves <terrell.alves@mscoding.org>
2025-07-20 10:10:53 -0500

    Finished Project
    Completed To Do List

--- ToDoList/src/components/Main.jsx
+++ ToDoList/src/components/Main.jsx
@@ -1,4 +1,7 @@
-import tododata from '../ToDoListData'
+import toDoData from '../ToDoListData'
+import {useState} from 'react'
+
+
 
 function Main() {
     
@@ -6,27 +9,14 @@ function Main() {
         <div className="row">
             <div className="col-12 text-center">
                 <h1>To Do List</h1>
+                <br />
+                <br />
+            </div>
+            <div className="col-12 text-center">
+                <h3>Activities</h3>
             </div>
             <div className="col-12">
-                <OpenList />
-                <br />
-                <br />
                 <Data />
-            </div>
-        </div>
-    )
-}
-
-function OpenList() {
-
-    function showData() {
-        
-    }
-
-    return(
-        <div className="row box">
-            <div className="col-12 text-center">
-                <button onClick={showData} className='listButton'>Open List</button>
             </div>
         </div>
     )
@@ -34,13 +24,35 @@ function OpenList() {
 
 function Data() {
 
+    const listData = toDoData.map((currentData) => {return <ItemData listName = {currentData.name} desc = {currentData.description} time = {currentData.timeDue} />})
 
     return(
-        <div className="row data">
-            <div className="col-12 text-center">
-                <h3>Activities</h3>
+        <div className="row itemData">
+            {listData}
+        </div>
+    )
+}
+
+function ItemData(props) {
+    
+    return (
+        <div className="row">
+            <div className="col-6">
+                <br />
+                <p>Chore: {props.listName}</p>
+                <p>Time Due: {props.time}</p>
+                <br />
+            </div>
+            <div className="col-4">
+                <br />
+                <p>Desc: {props.desc}</p>
+            </div>
+            <div className="col-2">
+                <br />
+                <button className='compButton'>Complete</button>
             </div>
         </div>
+
     )
 }
 
